3da05678932c44296222da554e2d2f6237f746b8
tjshiu <tiffany.shiu.2@gmail.com>
2018-10-02 17:06:06 -0700

    added routes

--- Inferno/config/routes.rb
+++ Inferno/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
-    resources :apps, only: [:index, :show]
+    resources :apps, only: [:create, :delete, :show, :new]
   end
 end
