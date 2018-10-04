d0ec5485f7a29d03da4ddd7211b211da387e5526
tjshiu <tiffany.shiu.2@gmail.com>
2018-10-04 15:43:07 -0700

    created app index

--- Inferno/frontend/src/Components/Apps/appsIndex.js
+++ Inferno/frontend/src/Components/Apps/appsIndex.js
@@ -4,20 +4,46 @@ import axios from 'axios';
 class AppIndex extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isLoaded: false,
+            apps: [],
+            error: null
+        };
     }
 
     componentDidMount() {
-        axios.get('/api/1/apps')
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+        axios.get('/api/1/apps/')
+            .then(response => {
+                let data = response.data;
+                let apps = Object.keys(data).map(key => data[key]);
+                this.setState({
+                    isLoaded: true,
+                    apps,
+                });
+                console.log(this.state);
+            })
+            .catch(error => this.setState({ isLoaded: true, error}));
     }
 
     render() {
-        return (
-        <div className="hello">
-            <div>hiiiiii</div>
-        </div>
-        );
+        let { error, isLoaded, apps} = this.state;
+        if (error) {
+            return <div>Error: {error.message} </div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+            <div className="hello">
+                <ul>
+                    {apps.map(app => (
+                        <li key={app.id}>
+                            {app.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            );
+        }
     }
 }
 
